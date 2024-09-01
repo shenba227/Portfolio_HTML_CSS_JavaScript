ce28bd637544815e8539726ecbc2d2956eaaf288
shenba227 <shenba227@gmail.com>
2024-09-01 20:54:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle the navigation menu on mobile//
+document.getElementById('hamburger-menu').addEventListener('click', function() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+});
